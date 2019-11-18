8ac6985e9773702c7f2bc7d200a3e689b28eee03
ghigbie <gehigbie@gmail.com>
2019-11-17 20:15:42 -0700

    Add more anotations

--- src/main/java/com/georgehigbie/bookmanager/model/Author.java
+++ src/main/java/com/georgehigbie/bookmanager/model/Author.java
@@ -1,9 +1,6 @@
 package com.georgehigbie.bookmanager.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,6 +13,7 @@ public class Author {
     private String firstName;
     private String lastName;
 
+    @ManyToMany
     private Set<Book> books = new HashSet<>();
 
     public Author(String firstName, String lastName) {
