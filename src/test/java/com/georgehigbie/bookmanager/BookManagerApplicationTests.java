48e3dca710d6b0eb36ee4692dd597581bae52053
ghigbie <gehigbie@gmail.com>
2019-11-17 19:53:12 -0700

    Initial commit

---
+++ src/test/java/com/georgehigbie/bookmanager/BookManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgehigbie.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
